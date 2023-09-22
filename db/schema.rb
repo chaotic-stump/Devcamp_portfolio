c7554b8e63b12c5f9ffd2e7265fc6ba65d78a00d
Sean Parker <seanrichardparker@gmail.com>
2023-09-22 14:26:00 -0400

    update portfolio to include attribute for position

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_210101) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_181124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_210101) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "position"
     t.index ["slug"], name: "index_portfolios_on_slug", unique: true
   end
 
