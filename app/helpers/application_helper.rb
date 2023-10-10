54923eaf4f7258dfa02c45d1db13519106e5451e
Sean Parker <seanrichardparker@gmail.com>
2023-10-10 18:21:00 -0400

    background cover image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: "#{style} text-secondary text-decoration-underline fw-bold") +
+      (link_to "Register", new_user_registration_path, class: "#{style}") +
       " ".html_safe + 
-      (link_to "Login", new_user_session_path, class: "#{style} text-secondary text-decoration-underline fw-bold")
+      (link_to "Login", new_user_session_path, class: "#{style}")
     else
-      link_to "Logout",  destroy_user_session_path, data: { turbo_method: :delete, turbo_confirm: 'Are you sure?' }, class: "#{style} text-secondary text-decoration-underline fw-bold"
+      link_to "Logout",  destroy_user_session_path, data: { turbo_method: :delete, turbo_confirm: 'Are you sure?' }, class: "#{style}"
     end
   end
 
@@ -45,7 +45,7 @@ module ApplicationHelper
     nav_links = ''
 
     nav_items.each do |item|
-      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} text-secondary text-decoration-underline fw-bold #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
     nav_links.html_safe
